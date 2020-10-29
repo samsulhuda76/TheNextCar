3072bafba8f0c5643f4deb2db8aa18545f297bb8
samsulhuda76 <61945288+samsulhuda76@users.noreply.github.com>
2020-10-29 17:35:26 +0700

    Pengenalan MVC

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheNextCar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnStartButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("OnLockDoorButtonClicked");
+
+        }
+
+        private void OnDoorOpenButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("OnDoorOpenButtonClicked");
+
+        }
+
+        private void OnLockDoorButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("OnLockDoorButtonClick");
+
+        }
+
+        private void OnAccuButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("OnAccuButtonClicked");
+
+        }
+    }
+}
